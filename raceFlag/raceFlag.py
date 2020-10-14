687894494d88fcba190c0099ed249009eaf5e591
Andy Lok <andylokon@gmail.com>
2020-10-15 08:42:46 +1100

    Ugly, but working

--- raceFlag/raceFlag.py
+++ raceFlag/raceFlag.py
@@ -20,21 +20,16 @@ APP_NAME = "Race Flag"
 TEXTURE_DIR = "apps/python/raceFlag/textures/"
 HEADER_HEIGHT = 30
 PADDING = 5 
+
 # Configs
 WINDOW_WIDTH = 160
 WINDOW_HEIGHT = 120
-SHOW_FLAG_NAME = False
 BACKGROUND_OPACITY = 0.5
-
-# Global variables
-flag_label = None
-appWindow = None
-previous_flag_value = None
 
 # Generate flags
 Flag = collections.namedtuple("Flag", ["name", "texture"])
 FLAGS = {
-    AC_NO_FLAG:        Flag(name="No flag",   texture="blue.png"),
+    AC_NO_FLAG:        Flag(name="No flag",   texture="empty.png"),
     AC_BLUE_FLAG:      Flag(name="Blue",      texture="blue.png"),
     AC_YELLOW_FLAG:    Flag(name="Yellow",    texture="yellow.png"),
     AC_BLACK_FLAG:     Flag(name="Black",     texture="black.png"),
@@ -43,39 +38,42 @@ FLAGS = {
     AC_PENALTY_FLAG:   Flag(name="Penalty",   texture="black_white.png"),
 }
 
-FLAG_TEXTURES = {
-    flag_value: ac.newTexture(TEXTURE_DIR + flag.texture)
-    for flag_value, flag in FLAGS.items()
-}
+# Global variables
+appWindow = None
+flag_textures = None
     
     
 def acMain(ac_version):
-    global flag_label, appWindow
+    global appWindow, flag_textures
 
     # Initialise app
     appWindow = ac.newApp(APP_NAME)
     ac.setSize(appWindow, WINDOW_WIDTH, WINDOW_HEIGHT)
+    ac.setIconPosition(appWindow, 0, -999999)
+    ac.setTitle(appWindow, " ")
     ac.drawBorder(appWindow, 0)
     ac.setBackgroundOpacity(appWindow, BACKGROUND_OPACITY)
+    ac.addRenderCallback(appWindow, onWindowRender)
 
-    # Add flag label text widget
-    flag_label = ac.addLabel(appWindow, "")
-    ac.setPosition(flag_label, PADDING, HEADER_HEIGHT)
+    # Load flag textures
+    flag_textures = {
+        flag_value: ac.newTexture(TEXTURE_DIR + flag.texture)
+        for flag_value, flag in FLAGS.items()
+    }
     
     return APP_NAME
     
 def acUpdate(deltaT):
-    global flag_label, appWindow, previous_flag_value
+    global appWindow
 
     ac.setBackgroundOpacity(appWindow, BACKGROUND_OPACITY)
 
+
+def onWindowRender(deltaT):
+    global flag_textures
+
     flag_value = info.graphics.flag
-    if flag_value != previous_flag_value:
-        if SHOW_FLAG_NAME:
-            flag_name = FLAGS[flag_value].name
-            ac.setText(flag_label, flag_name)
+    texture_id = flag_textures[flag_value]
+    ac.glColor4f(1.0, 1.0, 1.0, 1.0)
+    ac.glQuadTextured(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, texture_id)
 
-        texture_id = FLAG_TEXTURES[flag_value]
-        ac.glQuadTextured(0, HEADER_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT - HEADER_HEIGHT, texture_id)
-
-    previous_flag_value = flag_value
