a621f1c480fe2c1615d88ec0e84232988db53c41
Andy Lok <andy@lokcon.me>
2020-10-15 08:04:26 +1100

    Dirt: Switch to use texture

--- raceFlag/raceFlag.py
+++ raceFlag/raceFlag.py
@@ -1,5 +1,6 @@
 import ac
 import acsys
+import collections
 from utils import load_shared_libraries
 load_shared_libraries()
 from sim_info import info
@@ -12,69 +13,40 @@ AC_YELLOW_FLAG = 2
 AC_BLACK_FLAG = 3
 AC_WHITE_FLAG = 4
 AC_CHECKERED_FLAG = 5
-AC_PENALTY_FLAG = 6
+AC_PENALTY_FLAG = 6 
 
 # Constants
 APP_NAME = "Race Flag"
 TEXTURE_DIR = "apps/python/raceFlag/textures/"
-
+HEADER_HEIGHT = 30
+PADDING = 5 
 # Configs
 WINDOW_WIDTH = 160
 WINDOW_HEIGHT = 120
 SHOW_FLAG_NAME = False
-BACKGROUND_OPACITY = 0.1
+BACKGROUND_OPACITY = 0.5
 
 # Global variables
 flag_label = None
 appWindow = None
-    
+previous_flag_value = None
 
-def flag_to_name(flag_value):
-    if flag_value == AC_NO_FLAG:
-        return "No Flag"
-    elif flag_value == AC_BLUE_FLAG:
-        return "Blue"
-    elif flag_value == AC_YELLOW_FLAG:
-        return "Yellow"
-    elif flag_value == AC_BLACK_FLAG:
-        return "Black"
-    elif flag_value == AC_WHITE_FLAG:
-        return "White"
-    elif flag_value == AC_CHECKERED_FLAG:
-        return "Checkered"
-    elif flag_value == AC_PENALTY_FLAG:
-        return "Penality"
-    raise ValueError("Unknown racing flag value '{}'".format(flag_value))
-    
+# Generate flags
+Flag = collections.namedtuple("Flag", ["name", "texture"])
+FLAGS = {
+    AC_NO_FLAG:        Flag(name="No flag",   texture="blue.png"),
+    AC_BLUE_FLAG:      Flag(name="Blue",      texture="blue.png"),
+    AC_YELLOW_FLAG:    Flag(name="Yellow",    texture="yellow.png"),
+    AC_BLACK_FLAG:     Flag(name="Black",     texture="black.png"),
+    AC_WHITE_FLAG:     Flag(name="White",     texture="white.png"),
+    AC_CHECKERED_FLAG: Flag(name="Checkered", texture="chequered.png"),
+    AC_PENALTY_FLAG:   Flag(name="Penalty",   texture="black_white.png"),
+}
 
-def set_bg_texture_by_flag(appWindow, flag_value):
-    texture_file = ""
-    if flag_value == AC_NO_FLAG:
-        texture_file = "empty.png"
-    elif flag_value == AC_BLUE_FLAG:
-        texture_file = "blue.png"
-    elif flag_value == AC_YELLOW_FLAG:
-        texture_file = "yellow.png"
-    elif flag_value == AC_BLACK_FLAG:
-        texture_file = "black.png"
-    elif flag_value == AC_WHITE_FLAG:
-        texture_file = "white.png"
-    elif flag_value == AC_CHECKERED_FLAG:
-        texture_file = "chequered.png"
-    elif flag_value == AC_PENALTY_FLAG:
-        texture_file = "black_white.png"
-    else:
-        raise ValueError("Unknown racing flag value '{}'".format(flag_value))
-    
-    texture_path = TEXTURE_DIR + texture_file
-    
-    ac.setBackgroundTexture(appWindow, texture_path)
-    ac.setBackgroundOpacity(appWindow, BACKGROUND_OPACITY)
-
-
-def set_flag_label_by_flag(flag_label, flag_value):
-    if SHOW_FLAG_NAME:
-        ac.setText(flag_label, flag_to_name(flag_value))
+FLAG_TEXTURES = {
+    flag_value: ac.newTexture(TEXTURE_DIR + flag.texture)
+    for flag_value, flag in FLAGS.items()
+}
     
     
 def acMain(ac_version):
@@ -88,19 +60,22 @@ def acMain(ac_version):
 
     # Add flag label text widget
     flag_label = ac.addLabel(appWindow, "")
-    ac.setPosition(flag_label, 3, 30)
-
-    # Test texture
-    # texture = ac.newTexture(TEXTURE_DIR + "blue.png")
-    # ac.setPosition(texture, 0, 30)
+    ac.setPosition(flag_label, PADDING, HEADER_HEIGHT)
     
     return APP_NAME
     
-    
 def acUpdate(deltaT):
-    global flag_label, appWindow
+    global flag_label, appWindow, previous_flag_value
 
-    flag = info.graphics.flag
+    ac.setBackgroundOpacity(appWindow, BACKGROUND_OPACITY)
 
-    set_bg_texture_by_flag(appWindow, flag)
-    set_flag_label_by_flag(flag_label, flag)
+    flag_value = info.graphics.flag
+    if flag_value != previous_flag_value:
+        if SHOW_FLAG_NAME:
+            flag_name = FLAGS[flag_value].name
+            ac.setText(flag_label, flag_name)
+
+        texture_id = FLAG_TEXTURES[flag_value]
+        ac.glQuadTextured(0, HEADER_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT - HEADER_HEIGHT, texture_id)
+
+    previous_flag_value = flag_value
